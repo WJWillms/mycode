cb1f07da462e68c441651a1da985cbf842934cf8
WJWillms <wadejwillms@gmail.com>
2023-04-12 20:53:56 +0000

    Looping techniques

---
+++ Lab43/looping03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Looping across range() to generate n UUIDS
+# standard library import
+# allows us to generate UUIDs
+import uuid
+
+howmany = int(input("How many UUIDs should be generated? "))
+
+print("Generating UUIDs...")
+
+# range is required because an int cannot be looped
+for rando in range(howmany):
+    print( uuid.uuid4() )   # each time through the loop produce a UUID
