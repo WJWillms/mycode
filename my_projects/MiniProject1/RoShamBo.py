926bcaa7ecaf30a4cfee16df9c2388f08c20c607
WJWillms <wadejwillms@gmail.com>
2023-04-13 22:43:52 +0000

    RoShamBo Comments and hard mode update.

--- my_projects/MiniProject1/RoShamBo.py
+++ my_projects/MiniProject1/RoShamBo.py
@@ -6,6 +6,7 @@ import sys
 import tty
 import random
 
+#Splash Page for game
 def greeter():
     clearscreen()
     message = "Welcome to Rock Paper Scissors. The objective of the game is to beat your opponent by choosing the card that will beat your opponents.\nRock beats Scissors, Scissors beats Paper, and Paper beats Rock.\nYou have 5 of each card.You win by beating your opponent 3 times, you lose if your opponent wins 3 times, and draw if you run out of cards.\nPress any button to begin!"
@@ -14,10 +15,12 @@ def greeter():
         time.sleep(0.04)
     keypress()
     clearscreen()
-    
+
+#Clears the screen
 def clearscreen():
     print("\033c")
 
+#This function allows the press any button to continue feature
 def keypress():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -27,9 +30,13 @@ def keypress():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
-def cardChoice(Cards):
+#Function to allow user to pick a card
+def cardChoice(Cards, CPU_Cards, hard_Mode):
     validCard=False
     while validCard == False:
+        if hard_Mode == False:
+            print("Opponent cards left: " + str(CPU_Cards[0]) + " Rock " + str(CPU_Cards[1]) + " Paper " + str(CPU_Cards[2]) + " Scissors ")
+
         print("What card would you like to play?")
         print("1. Rock (You have " + str(Cards[0]) + " cards left)")
         print("2. Paper (You have " + str(Cards[1]) + " cards left)")
@@ -62,6 +69,7 @@ def cardChoice(Cards):
             time.sleep(1)
             clearscreen()
 
+#Function for CPU to choose a card
 def CPUcardChoice(Cards):
     validCard=False
     while validCard == False:
@@ -69,6 +77,7 @@ def CPUcardChoice(Cards):
         if Cards[CPUchoice] > 0:
             return CPUchoice
 
+#Function for comparing cards to find winner
 def Battle(user, CPU):
     print("Opponents Card")
     print("-----------------------------------------")
@@ -112,41 +121,44 @@ def Battle(user, CPU):
     elif user == 2 and CPU == 1:
         print("You won the round!")
         return 1
-    elif user == 2 and CPU == 2:
+    else:
         print("The round is a Draw!")
         return 3
     
-           
+ #Print paper ASCII          
 def PaperChoice():
     print("""
      _______
 ---'    ____)____
-           ______)
-          _______)
+           ______)     
+          _______)     PAPER
          _______)
 ---.__________)
 """)
 
+#Print Rock ASCII
 def RockChoice():
     print("""
     _______
 ---'   ____)
       (_____)
-      (_____)
+      (_____)          Rock
       (____)
 ---.__(___)
 """)
 
+#Print Scissor ASCII
 def ScissorChoice():
     print("""
     _______
 ---'   ____)____
           ______)
-       __________)
+       __________)     SCISSORS 
       (____)
 ---.__(___)
 """)
 
+#Print Lives remaing and card remaining
 def LifeUpdate(user,CPU,cards):
     print("CPU Lives: " + str(CPU))
     print("Your Lives: " + str(user))
@@ -154,6 +166,20 @@ def LifeUpdate(user,CPU,cards):
     print("Press any button to proceed to the next round")
     keypress()
     clearscreen()
+#Function to ask if you want the game to output what cards the CPU has left
+def HardModeActivation():
+    x=""
+    while x != '1' and x != '2':
+        print("The game has a small dificulty modifier. Choose which way to play")
+        print("1. Show amount of CPU cards left")
+        print("2. Don't show amount of CPU cards left")
+        x = input("Mode: ")
+        clearscreen()
+    
+    if x == '1':
+        return False
+    else:
+        return True
 
 def main():
     #Numbers in list coordinate to Rock, Paper, Scissors(Respectively) and the amount of that card that is left
@@ -167,9 +193,11 @@ def main():
         user_Lives = 3
         CPU_Lives = 3
         gameContinue = True
+        #Ask for Hard mode
+        hard_Mode = HardModeActivation()
         while gameContinue == True:    
             #Getting User Card
-            user_Choice = cardChoice(user_Cards) 
+            user_Choice = cardChoice(user_Cards, CPU_Cards, hard_Mode) 
             #Getting CPU Card
             CPU_Choice = CPUcardChoice(CPU_Cards)
             #Calc Winner
@@ -204,11 +232,6 @@ def main():
             if user_Lives > 0 and CPU_Lives > 0 and Card_Count > 0:
                 LifeUpdate(user_Lives, CPU_Lives, Card_Count)
 
-
-
-
-    
-    
-
+#Ro Sham Bo
 if __name__ == '__main__':
     main()
